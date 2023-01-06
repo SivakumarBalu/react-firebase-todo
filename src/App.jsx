f80a9f724fac61d3bbdb0ac639b23a4bde5da226
Thejashvi Sivakumar <theju@Air.local>
2023-01-05 21:29:25 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { addDoc, collection, onSnapshot, query, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import Todo from './components/Todo';
+import { db } from './firebase';
+// import {query, collection } from 'firebase/firestore'
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
+};
+
+function App() {
+  const [todos, setTodos] = useState(['Learn React', 'Learn Vue'])
+  const [input, setInput] = useState('')
+  //Create Todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === '') {
+      alert('Please enter a valid todo');
+      return;
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
+    });
+    setInput('');
+  }
+  //Read Todo
+  useEffect(() => {
+    const q = query(collection(db, 'todos'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todoArr = []
+      querySnapshot.forEach((doc) => {
+        todoArr.push({...doc.data(), id:doc.id})
+      })
+      setTodos(todoArr)
+    })
+    return () => unsubscribe()
+  },[])
+
+  const todoComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed,
+    })
+  }
+
+  // Delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id));
+  };
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <div className={style.heading}>
+          <h1>Todo List</h1>
+        </div>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type='text'
+            placeholder='Add Todo'
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} todoComplete={todoComplete} deleteTodo={deleteTodo}/>
+          ))}
+        </ul>
+      </div>
+      {todos.length < 1 ? null : <p className={style.count}>{`You have ${todos.length} todos`}</p>}
+      
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ["firebase", "firebase/app", "firebase/auth", "firebase/firestore", "firebase/analytics"],
+  },
+  plugins: [react(),tailwindcss()],
+})
